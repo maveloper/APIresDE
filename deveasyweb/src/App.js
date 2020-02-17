f54769203253a9a27cb7232985262de2ff2fee3c
Maveloper <mavcode@outlook.com>
2020-02-17 10:38:58 -0300

    Add front-end layer

---
+++ deveasyweb/src/App.js
@@ -0,0 +1,120 @@
+import React, {useEffect} from 'react';
+import './Global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+
+function App() {
+useEffect(() =>{
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+    },
+    (err) => {
+      console.log(err);
+    },
+    {
+      timeout: 30000,
+    }
+  )
+
+},[]);
+
+
+    return (
+   <div id="app">
+     <aside>
+      <strong>Register</strong>
+      <form>
+        <div className="input-block">
+          <label htmlFor="github_username">Github Username</label>
+            <input name="github_username" id="github_username" required/>
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="techs">Your tech skills</label>
+          <input name="techs" id="techs" required/>
+        </div>
+
+        <div className="input-group">
+          <div className="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id= "Latitude" required/>  
+        </div>
+
+        <div className="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+        </div>
+        </div>
+ 
+        <button type="submit">Save</button>
+
+      </form>
+     </aside>
+
+     <main>
+       <ul>
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars2.githubusercontent.com/u/57489512?s=460&v=4" alt="May Santos"/>
+             <div className="user-info">
+               <strong>May Santos</strong>
+               <span>Node.jS, ReactJS, React Native,Python </span>
+             </div>
+           </header>
+           <p>JS/ReactJS/Python - I'm a very curious person and passionate about creations, interactivity on multiplatforms and solve problems. Learning React Native</p>
+           <a href="https://github.com/maveloper">Access GitHub profile</a>
+         </li>
+
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars2.githubusercontent.com/u/57489512?s=460&v=4" alt="May Santos"/>
+             <div className="user-info">
+               <strong>May Santos</strong>
+               <span>Node.jS, ReactJS, React Native,Python </span>
+             </div>
+           </header>
+           <p>JS/ReactJS/Python - I'm a very curious person and passionate about creations, interactivity on multiplatforms and solve problems. Learning React Native</p>
+           <a href="https://github.com/maveloper">Access GitHub profile</a>
+         </li>
+
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars2.githubusercontent.com/u/57489512?s=460&v=4" alt="May Santos"/>
+             <div className="user-info">
+               <strong>May Santos</strong>
+               <span>Node.jS, ReactJS, React Native,Python </span>
+             </div>
+           </header>
+           <p>JS/ReactJS/Python - I'm a very curious person and passionate about creations, interactivity on multiplatforms and solve problems. Learning React Native</p>
+           <a href="https://github.com/maveloper">Access GitHub profile</a>
+         </li>
+
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars2.githubusercontent.com/u/57489512?s=460&v=4" alt="May Santos"/>
+             <div className="user-info">
+               <strong>May Santos</strong>
+               <span>Node.jS, ReactJS, React Native,Python </span>
+             </div>
+           </header>
+           <p>JS/ReactJS/Python - I'm a very curious person and passionate about creations, interactivity on multiplatforms and solve problems. Learning React Native</p>
+           <a href="https://github.com/maveloper">Access GitHub profile</a>
+         </li>
+
+
+       </ul>
+
+     </main>
+   </div> 
+   
+   );
+
+  
+}
+
+export default App;
+
+
